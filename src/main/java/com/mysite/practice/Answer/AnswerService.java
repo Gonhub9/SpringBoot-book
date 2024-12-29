afe04861fc212bf0352f770e3106a294c41b753f
Gonhub9 <ho_gok@naver.com>
2024-12-29 20:28:23 +0900

    css

---
+++ src/main/java/com/mysite/practice/Answer/AnswerService.java
@@ -0,0 +1,22 @@
+package com.mysite.practice.Answer;
+
+import com.mysite.practice.Question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
